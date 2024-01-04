89a35750f26482ad0366fc3fb3323bbc35186eb2
David Wang <git@davidwa.ng>
2024-01-03 22:20:46 -0600

    neovim debugging configuration

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,41 @@
 local plugins = {
   {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
     "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     opts = function()
@@ -19,6 +55,7 @@ local plugins = {
       ensure_installed = {
         "clangd",
         "clang-format",
+        "codelldb",
       }
     }
   }
